2dc6c4a42f6fd2836a6bbf37ccc6790f82720fd6
qStivi <stephanglaue@googlemail.com>
2024-09-15 19:30:19 +0200

    Initial commit from local project

---
+++ src/main/java/com/qStivi/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Stephan Glaue
+ *
+ * This source code is licensed under the MIT License found in the LICENSE file in the root directory of this source tree.
+ */
+
+package com.qStivi;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
